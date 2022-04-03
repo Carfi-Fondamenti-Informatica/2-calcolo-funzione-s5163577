b34efecd2e146c42b974985cbf0534cfbe82d5c0
s5163577 <100301188+s5163577@users.noreply.github.com>
2022-04-03 21:17:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   float a=0, b=0, x=0, y=0, z=0;
+   
+   cin >> a >> b >> x >> y;
+   
+   if (x<0 && y>0) {
+      z = (a*x) - (b*y);
+   }
+   else if (x>=0 && y<=0) {
+      z = (a*(x*x)) - (b*y);
+   }
+   else {
+      z = (a*x) + (b*(y*y));
+   }
+   cout << z << endl;
    return 0;
 }
